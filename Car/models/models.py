f5233bb8d4cb8b5f1a946e5bf21269fa1c68f9fa
jmlm74 <jmlm74@gmail.com>
2021-02-08 22:25:59 +0100

    Add field One2many

--- Car/models/models.py
+++ Car/models/models.py
@@ -10,3 +10,13 @@ class Car(models.Model):
     door_number = fields.Integer(string='Door Number')
 
     driver_id = fields.Many2one('res.partner', string='Driver')
+    parking_id = fields.Many2one('parking.parking', string='Parking')
+
+
+# One2Many relation
+class Parking(models.Model):
+    _name = 'parking.parking'
+    _description = 'parking for cars '
+
+    name = fields.Char(string="Parking name")
+    car_ids = fields.One2many('car.car', 'parking_id', string='Cars')
