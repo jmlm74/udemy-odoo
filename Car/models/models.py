54bdd59d8d40ef2f2d0f40f518cabf22d192f06a
jmlm74 <jmlm74@gmail.com>
2021-02-09 16:22:54 +0100

    Create form buttons

--- Car/models/models.py
+++ Car/models/models.py
@@ -9,19 +9,30 @@ class Car(models.Model):
     horse_power = fields.Integer(string='Horse Power')
     door_number = fields.Integer(string='Door Number')
 
-    total_speed=fields.Integer(string='Total speed (computed)', compute='get_total_speed' )
+    total_speed = fields.Integer(string='Total speed (computed)', compute='get_total_speed')
 
     driver_id = fields.Many2one('res.partner', string='Driver')
     parking_id = fields.Many2one('parking.parking', string='Parking')
     feature_ids = fields.Many2many('car.feature', string='Feature')
 
+    state = fields.Selection([('new', 'New'),
+                              ('used', 'Used'),
+                              ('sold', 'Sold')], string='Status', default='new')
+
     """
-    Functions
+    Methods
     """
+
     # computed speed
     def get_total_speed(self):
         for rec in self:
             rec.total_speed = rec.horse_power + 100
+
+    def set_car_to_used(self):
+        self.state = 'used'
+
+    def set_car_to_sold(self):
+        self.state = 'sold'
 
 # One2Many relation
 class Parking(models.Model):
@@ -31,10 +42,10 @@ class Parking(models.Model):
     name = fields.Char(string="Parking name")
     car_ids = fields.One2many('car.car', 'parking_id', string='Cars')
 
+
 # Many2Many relation
 class CarFeatures(models.Model):
     _name = 'car.feature'
     _description = 'Features for cars '
 
     name = fields.Char(string="Feature name")
-
