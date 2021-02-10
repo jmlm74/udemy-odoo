c483dfcf20309520b6f30863485d58a5a675d317
jmlm74 <jmlm74@gmail.com>
2021-02-10 16:26:00 +0100

    Add smart button

--- Car/models/partner.py
+++ Car/models/partner.py
@@ -6,4 +6,18 @@ class ResPartnerInherit(models.Model):
 
     message_car = fields.Char(string='message_car')
 
+    car_count=fields.Integer(string='Count',compute='get_car_counter')
 
+    def get_cars(self):
+        self.ensure_one()
+        return {'type': 'ir.actions.act_window',
+                'name': 'cars',
+                'view_mode': 'tree',
+                'res_model': 'fleet.vehicle',
+                'domain': [('driver_id', '=', self.id)],
+                'context': "{'create': False}"
+               }
+
+    def get_car_counter(self):
+        for rec in self:
+            rec.car_count=self.env['fleet.vehicle'].search_count([('driver_id', '=', self.id)])
